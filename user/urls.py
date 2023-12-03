969813ac8ccd9672c0a7d3330c5d2ac100e5728f
Gabriel Silva <gabrielsilvacarvalho@usp.br>
2023-12-02 23:50:46 -0300

    só passando os estilos para o css basicamente

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
 from user.views import Registrar
 from django.urls import path, include
+from django.contrib.auth.views import PasswordResetView
 
 urlpatterns = [
                path('registrar/', Registrar, name = 'registrar'),
+               path('recuperar-senha/',PasswordResetView.as_view() ,name = "recuperar-senha"),
                path('', include('django.contrib.auth.urls')),
 ]
