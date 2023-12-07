69e739e50eaf7c11458888ddff2f79c8778461f7
Breno <brenobraga2003@usp.br>
2023-12-07 20:29:02 -0300

    Mudança no estilo de pesquisa de locais/adicionar locais

--- local/forms.py
+++ local/forms.py
@@ -2,8 +2,16 @@ from django import forms
 from local.models import Local
 
 class AddLocalForm(forms.ModelForm):
-        class Meta:
-            model = Local
-            fields = ['nome','endereço','descrição','CEP','latitude','longitude','imagem']
-            labels = {'nome':'Nome'}
-            widgets = {'descrição':forms.Textarea}
+    class Meta:
+        model = Local
+        fields = ['nome', 'endereço', 'descrição', 'CEP', 'latitude', 'longitude', 'imagem']
+        labels = {'nome': 'Nome'}
+        widgets = {
+            'nome': forms.TextInput(attrs={'placeholder': 'Nome'}),
+            'descrição': forms.Textarea(attrs={'placeholder': 'Descrição'}),
+            'endereço': forms.TextInput(attrs={'placeholder': 'Endereço'}),
+            'CEP': forms.TextInput(attrs={'placeholder': 'CEP'}),
+            'latitude': forms.TextInput(attrs={'placeholder': 'Latitude'}),
+            'longitude': forms.TextInput(attrs={'placeholder': 'Longitude'}),
+            'imagem': forms.ClearableFileInput(attrs={'placeholder': 'Imagem'}),
+        }
